1f1fe0d366cb1bb941408239c53b232a4e37f95a
MadinaBahriddinova <127374347+MadinaBahriddinova@users.noreply.github.com>
2023-12-13 23:52:42 +0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -57,7 +57,19 @@ int main() {
     
     
     std::cout << "Task 4" << std::endl;
-    // call for task 4
+    std::cout << "Enter the number of nodes: ";
+    std::cin >> N;
+
+    std::cout << "Enter the values of nodes: ";
+    for (int i = 0; i < N; ++i) {
+        std::cin >> value;
+        tree.insert(value);
+    }
+
+    std::cout << "Tree Structure:" << std::endl;
+    tree.printTree();
+    
+    
     std::cout << "Task 5" << std::endl;
     // call for task 5
     return 0;
