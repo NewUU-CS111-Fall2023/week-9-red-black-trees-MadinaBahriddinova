5af1b2886d93b9c0e49f50271e97bbad5a5cbbf9
MadinaBahriddinova <127374347+MadinaBahriddinova@users.noreply.github.com>
2023-12-13 22:29:47 +0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,26 @@
 
 int main() {
     std::cout << "Task 1" << std::endl;
-    // call for task 1
+    RedBlackTree tree;
+
+    // Given tree nodes
+    tree.insert(9);
+    tree.insert(10);
+    tree.insert(13);
+    tree.insert(3);
+    tree.insert(6);
+    tree.insert(2);
+    tree.insert(7);
+    tree.insert(5);
+
+    std::cout << "Initial Red-Black Tree (inorder traversal):" << std::endl;
+    tree.inorder();
+
+    // Inserting 4 to the tree
+    tree.insert(4);
+    std::cout << "Red-Black Tree after inserting 4 (inorder traversal):" << std::endl;
+    tree.inorder();
+
     std::cout << "Task 2" << std::endl;
     // call for task 2
     std::cout << "Task 3" << std::endl;
